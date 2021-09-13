1697bd8cf9a8d6cd083ffe99c299e253eb4ad23d
Kayleigh Ellis <elliskay@kayleighs-imac.home>
2021-09-13 19:41:51 +0100

    kpi calculator added with custom js and is fully working. Added pexels API integration. Need to work on styling photos page and adding contact/info page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,19 @@
+
+ function round(num) {
+     var m=Number((Math.abs(num)*100).toPrecision(15));
+     return Math.round(m) / 100 * Math.sign(num);
+ }  
+
 function calculateCTR () {
     var operand1=document.getElementById("operand1").value;
     var operand2=document.getElementById("operand2").value;
     var operand3=document.getElementById("operand3").value;
     var operand4=document.getElementById("operand4").value;
 
-    var result=parseInt(operand3) / parseInt(operand2) *100; 
+    var result=round(operand3/operand2*100); 
 
     if(!isNaN(result)){
-        document.getElementById("ctr").textContent= result;
+        document.getElementById("ctr").textContent= result + "%";
     }
 }
 
@@ -17,10 +23,10 @@ function calculateCPC () {
     var operand3=document.getElementById("operand3").value;
     var operand4=document.getElementById("operand4").value;
 
-    var result=parseInt(operand1) / parseInt(operand3); 
+    var result=round(operand1/operand3); 
 
     if(!isNaN(result)){
-        document.getElementById("cpc").textContent= result;
+        document.getElementById("cpc").textContent= "£" +result;
     }
 }
 
@@ -30,10 +36,10 @@ function calculateCPL () {
     var operand3=document.getElementById("operand3").value;
     var operand4=document.getElementById("operand4").value;
 
-    var result=parseInt(operand1) / parseInt(operand4); 
+    var result=round(operand1/operand4); 
 
     if(!isNaN(result)){
-        document.getElementById("cpl").textContent= result;
+        document.getElementById("cpl").textContent= "£" +result;
     }
 }
 
@@ -43,9 +49,13 @@ function calculateCVR () {
     var operand3=document.getElementById("operand3").value;
     var operand4=document.getElementById("operand4").value;
 
-    var result=parseInt(operand4) / parseInt(operand3) *100; 
+    var result= round(operand4/operand3*100); 
 
     if(!isNaN(result)){
-        document.getElementById("cvr").textContent= result;
+        document.getElementById("cvr").textContent= result + "%";
     }
 }
+
+
+
+
